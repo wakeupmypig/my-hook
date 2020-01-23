7bc68f4593f65598e4cfd68932f407de1b647f5f
cindy <l>
2020-01-23 16:34:07 +0800

    'hook'

--- hook.js
+++ hook.js
@@ -1,9 +1,19 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const app = new Koa();
+const bodyparser = require('koa-bodyparser')
 const router = new Router();
-router.post('/webhook',function (ctx) {
-    console.log('post hook')
+const crypto = require('crypto');
+app.use(bodyparser());
+function s(body) {
+    return `sha1=`+ crypto.createHmac('sha1','').update(body).digest('hex');
+}
+router.post('/webhook',async function (ctx) {
+    let body = await ctx.request.body;
+    let event = ctx.get('x-github-event');
+    let signature = ctx.get('x-hub-signature');
+    let newSign = s(body);
+    console.log(event,signature,newSign);
     ctx.body = {ok:true}
 });
 app.use(router.routes());
